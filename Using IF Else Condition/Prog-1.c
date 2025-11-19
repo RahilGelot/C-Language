6b3fa08a4ca9b35df110647a6ed3804a7efabd62
RahilGelot <gelotrahil@gmail.com>
2025-11-19 23:48:14 +0530

    Implement program to find largest and smallest values
    This program prompts the user to enter two numbers and determines the largest and smallest values among them.

---
+++ Using IF Else Condition/Prog-1.c
@@ -0,0 +1,11 @@
+/* Find Largest and smallest of two values */
+#include <stdio.h>
+int main() {
+    double a,b;
+    printf("Enter two numbers: ");
+    if (scanf("%lf %lf",&a,&b)!=2) return 0;
+    if (a > b) printf("Largest = %.6g\nSmallest = %.6g\n", a, b);
+    else if (b > a) printf("Largest = %.6g\nSmallest = %.6g\n", b, a);
+    else printf("Both equal = %.6g\n", a);
+    return 0;
+}
