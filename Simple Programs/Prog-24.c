9d2679cbdd2b63268ee99e022fe42b3ab48f9236
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:04:20 +0530

    Update Prog-24.c

--- Simple Programs/Prog-24.c
+++ Simple Programs/Prog-24.c
@@ -1,11 +1,11 @@
 /* 24. Swap two values */
 #include <stdio.h>
-int main() {
+void main() {
     double a, b, temp;
     printf("Enter two values: ");
     if (scanf("%lf %lf", &a, &b)!=2) return 0;
     printf("Before swap: a=%.6g, b=%.6g\n", a, b);
     temp = a; a = b; b = temp;
     printf("After swap: a=%.6g, b=%.6g\n", a, b);
-    return 0;
+    return ;
 }
