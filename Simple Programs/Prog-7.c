8fa1c29e58848718aa60407e6205d3ec6fdbf51a
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:05:45 +0530

    Update Prog-7.c

--- Simple Programs/Prog-7.c
+++ Simple Programs/Prog-7.c
@@ -1,9 +1,9 @@
 /* 7. Convert minutes into hours */
 #include <stdio.h>
-int main() {
+void main() {
     double minutes;
     printf("Enter minutes: ");
     if (scanf("%lf", &minutes)!=1) return 0;
     printf("%.6g minutes = %.6g hours\n", minutes, minutes / 60.0);
-    return 0;
+    return ;
 }
