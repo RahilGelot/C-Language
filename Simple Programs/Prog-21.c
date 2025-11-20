eed09aa1606216bf0da31efbc919a08a2d3032aa
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:03:26 +0530

    Update Prog-21.c

--- Simple Programs/Prog-21.c
+++ Simple Programs/Prog-21.c
@@ -1,6 +1,6 @@
 /* 21. Net salary with allowances 10% and deductions 3% of gross */
 #include <stdio.h>
-int main() {
+void main() {
     double gross;
     printf("Enter gross salary: ");
     if (scanf("%lf", &gross)!=1) return 0;
@@ -8,5 +8,5 @@ int main() {
     double deduction = 0.03 * gross;
     double net = gross + allowance - deduction;
     printf("Allowance = %.6g\nDeduction = %.6g\nNet salary = %.6g\n", allowance, deduction, net);
-    return 0;
+    return ;
 }
