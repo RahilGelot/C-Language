cd38440824e3b88eca268381d661367de7b4b2bc
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:01:22 +0530

    Update Prog-14.c

--- Simple Programs/Prog-14.c
+++ Simple Programs/Prog-14.c
@@ -1,10 +1,10 @@
 /* 14. Convert Celsius into Fahrenheit */
 #include <stdio.h>
-int main() {
+void main() {
     double c;
     printf("Enter Celsius: ");
     if (scanf("%lf", &c)!=1) return 0;
     double f = (9.0/5.0) * c + 32.0;
     printf("%.6g C = %.6g F\n", c, f);
-    return 0;
+    return ;
 }
