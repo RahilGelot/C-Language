64639f149f742dfd1d3eafe28be4349573d36630
RahilGelot <gelotrahil@gmail.com>
2025-11-20 17:59:56 +0530

    Update Prog-10.c

--- Simple Programs/Prog-10.c
+++ Simple Programs/Prog-10.c
@@ -1,11 +1,11 @@
 /* 10. Convert dollars into pound (via Rs): 1$=48Rs, 1 pound=70Rs */
 #include <stdio.h>
-int main() {
+void main() {
     double dollars;
     printf("Enter amount in dollars: ");
     if (scanf("%lf", &dollars)!=1) return 0;
     double rs = dollars * 48.0;
     double pound = rs / 70.0;
     printf("%.6g $ = %.6g pounds\n", dollars, pound);
-    return 0;
+    return ;
 }
