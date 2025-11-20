3dd9a51017497864531b744d1728b89cdb3a6195
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:10:28 +0530

    Update Prog-1.c

--- Using Loop/Prog-1.c
+++ Using Loop/Prog-1.c
@@ -1,7 +1,7 @@
 /* LOOP-1 Print 1st 10 natural numbers */
 #include <stdio.h>
-int main() {
+void main() {
     for (int i=1;i<=10;i++) printf("%d ", i);
     printf("\n");
-    return 0;
+    return ;
 }
