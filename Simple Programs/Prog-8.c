7b4de4f565a79a88dd28d53e04c3f911bb75fa4d
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:05:59 +0530

    Update Prog-8.c

--- Simple Programs/Prog-8.c
+++ Simple Programs/Prog-8.c
@@ -1,9 +1,9 @@
 /* 8. Convert dollars into Rs (1$ = 48 Rs) */
 #include <stdio.h>
-int main() {
+void main() {
     double dollars;
     printf("Enter amount in dollars: ");
     if (scanf("%lf", &dollars)!=1) return 0;
     printf("%.6g $ = %.6g Rs\n", dollars, dollars * 48.0);
-    return 0;
+    return ;
 }
