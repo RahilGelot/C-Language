f8769eb12173ed0b4e90a3c07cfc4e2f67565ae8
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:02:48 +0530

    Update Prog-19.c

--- Simple Programs/Prog-19.c
+++ Simple Programs/Prog-19.c
@@ -1,10 +1,10 @@
 /* 19. Area of a circle (approx pi=22/7) */
 #include <stdio.h>
-int main() {
+void main() {
     double R;
     printf("Enter radius: ");
     if (scanf("%lf", &R)!=1) return 0;
     double area = (22.0/7.0) * R * R;
     printf("Area = %.6g\n", area);
-    return 0;
+    return ;
 }
