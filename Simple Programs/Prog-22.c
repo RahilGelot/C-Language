cb3c6b08444339a5dbe4ce50b1a8e36869e1e56c
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:03:46 +0530

    Update Prog-22.c

--- Simple Programs/Prog-22.c
+++ Simple Programs/Prog-22.c
@@ -1,10 +1,10 @@
 /* 22. Net sales = gross - 10% discount */
 #include <stdio.h>
-int main() {
+void main() {
     double gross;
     printf("Enter gross sales: ");
     if (scanf("%lf", &gross)!=1) return 0;
     double net = gross * (1.0 - 0.10);
     printf("Net sales = %.6g\n", net);
-    return 0;
+    return ;
 }
