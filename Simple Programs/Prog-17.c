9c0ea56bfcaf3e32fed86c1e1895d1d380851cfb
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:02:18 +0530

    Update Prog-17.c

--- Simple Programs/Prog-17.c
+++ Simple Programs/Prog-17.c
@@ -1,9 +1,9 @@
 /* 17. Area and perimeter of square */
 #include <stdio.h>
-int main() {
+void main() {
     double L;
     printf("Enter side length L: ");
     if (scanf("%lf", &L)!=1) return 0;
     printf("Area = %.6g\nPerimeter = %.6g\n", L*L, 4*L);
-    return 0;
+    return ;
 }
