4333fe3b53c4a8e27aa948e3f2750f831fb4a54c
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:04:03 +0530

    Update Prog-23.c

--- Simple Programs/Prog-23.c
+++ Simple Programs/Prog-23.c
@@ -1,11 +1,11 @@
 /* 23. Average of three subjects and total */
 #include <stdio.h>
-int main() {
+void main() {
     double a,b,c;
     printf("Enter marks of three subjects: ");
     if (scanf("%lf %lf %lf", &a, &b, &c)!=3) return 0;
     double total = a + b + c;
     double avg = total / 3.0;
     printf("Total = %.6g\nAverage = %.6g\n", total, avg);
-    return 0;
+    return ;
 }
