c03f60018884130dfb640d444ee2cc49d43599ec
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:01:02 +0530

    Update Prog-13.c

--- Simple Programs/Prog-13.c
+++ Simple Programs/Prog-13.c
@@ -1,10 +1,10 @@
 /* 13. Convert bytes into KB, MB and GB */
 #include <stdio.h>
-int main() {
+void main() {
     double bytes;
     printf("Enter bytes: ");
     if (scanf("%lf", &bytes)!=1) return 0;
     printf("%.0f bytes = %.6g KB = %.6g MB = %.6g GB\n",
            bytes, bytes/1024.0, bytes/(1024.0*1024.0), bytes/(1024.0*1024.0*1024.0));
-    return 0;
+    return ;
 }
