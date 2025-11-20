ac5d86409e3dc6af4c4b764f4788018ca8a82636
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:00:45 +0530

    Update Prog-12.c

--- Simple Programs/Prog-12.c
+++ Simple Programs/Prog-12.c
@@ -1,9 +1,9 @@
 /* 12. Convert kgs into grams */
 #include <stdio.h>
-int main() {
+void main() {
     double kg;
     printf("Enter kilograms: ");
     if (scanf("%lf", &kg)!=1) return 0;
     printf("%.6g kg = %.6g g\n", kg, kg * 1000.0);
-    return 0;
+    return ;
 }
