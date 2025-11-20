b61453ef7d0bb3816405c512d43ed8b51a8ba0fa
RahilGelot <gelotrahil@gmail.com>
2025-11-20 18:01:54 +0530

    Update Prog-16.c

--- Simple Programs/Prog-16.c
+++ Simple Programs/Prog-16.c
@@ -1,10 +1,10 @@
 /* 16. Calculate simple interest I = P*R*N/100 */
 #include <stdio.h>
-int main() {
+void main() {
     double P, R, N;
     printf("Enter principal, rate%% per year, years: ");
     if (scanf("%lf %lf %lf", &P, &R, &N)!=3) return 0;
     double I = (P * R * N) / 100.0;
     printf("Interest = %.6g\n", I);
-    return 0;
+    return ;
 }
